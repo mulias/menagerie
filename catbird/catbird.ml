58efc699703b6bd87c7332ebe491f06be74c1268
mulhalle <mulhalle@grinnell.edu>
2015-11-09 10:21:45 -0600

    add lists to catbird, modify eq to be case exhaustive

--- catbird/catbird.ml
+++ catbird/catbird.ml
@@ -1,11 +1,15 @@
 open Core.Std
 
 (* AST AND OUTPUT TYPES *)
-
-type exp =
+type const =
   | Int of int
   | Str of string
   | Bool of bool
+  | Sym of string
+  | List of const list
+
+type exp =
+  | Const of const
   | Var of string
   | Lambda of exp * exp
   | Apply of exp * exp
@@ -15,7 +19,9 @@ type out =
   | Int of int
   | Str of string
   | Bool of bool
+  | Sym of string
   | Fun of (out -> out)
+  | List of out list
   | Error
 
 (* HELLO ASSOC, MY OLD FRIEND *)
@@ -25,11 +31,18 @@ type env = (exp, out) List.Assoc.t
 (* HOW TO INTERPRET AN EXP IN AN ENV *)
 
 let rec interp (exp : exp) (env : env) : out =
-  let lookup (exp : exp) : out =
+  let rec convert_const (const : const) : out =
+    match const with
+    | Int i     -> Int i
+    | Str s     -> Str s
+    | Bool b    -> Bool b
+    | Sym s     -> Sym s
+    | List exps -> List (List.map exps convert_const)
+  in let lookup_env (exp : exp) : out =
     match List.Assoc.find env exp with
     | Some out -> out
     | _ -> Error
-  in let extend (arg : exp) (body :exp) : out =
+  in let extend_env (arg : exp) (body :exp) : out =
     match arg with
     | Var _ -> Fun (fun x -> (interp body ((arg, x) :: env)))
     | _ -> Error
@@ -43,11 +56,9 @@ let rec interp (exp : exp) (env : env) : out =
     | Bool false -> interp alt env
     | _ -> Error
   in match exp with
-  | Int i               -> Int i
-  | Str s               -> Str s
-  | Bool b              -> Bool b
-  | Var _               -> lookup exp
-  | Lambda (arg, body)  -> extend arg body
+  | Const c             -> convert_const c
+  | Var _               -> lookup_env exp
+  | Lambda (arg, body)  -> extend_env arg body
   | Apply (lambda, exp) -> apply lambda exp
   | If (test, res, alt) -> if_consequent test res alt
 
@@ -85,11 +96,18 @@ let from_bool (out : out) : bool option =
   | Bool b -> Some b
   | _ -> None
 
+let from_list (out : out) : out list option =
+  match out with
+  | List l -> Some l
+  | _ -> None
+
 let to_int (i : int) : out = Int i
 
 let to_str (s : string) : out = Str s
 
 let to_bool (b : bool) : out = Bool b
+
+let to_list (l : out list) : out = List l
 
 (* MATH *)
 let int_unary_op (op : (int -> int)) : out =
@@ -115,13 +133,16 @@ let bool_not = bool_unary_op (not)
 let bool_to_str = unary_fun from_bool Bool.to_string to_str
 let eq = 
   Fun (fun (o1 : out) -> Fun (fun (o2 : out) -> 
-    let b_val =
-      match (o1, o2) with
-      | (Int a, Int b)   -> a = b
-      | (Str a, Str b)   -> a = b
-      | (Bool a, Bool b) -> a = b
-      | _ -> false
-    in Bool b_val))
+    match (o1, o2) with
+    | (Int a, Int b)   -> Bool (a = b)
+    | (Str a, Str b)   -> Bool (a = b)
+    | (Bool a, Bool b) -> Bool (a = b)
+    | (Sym a, Sym b)   -> Bool (a = b)
+    | (List a, List b) -> Bool (a = b)
+    | (Int _, _)  | (Str _, _) | 
+      (Bool _, _) | (Sym _, _) | 
+      (Fun _, _)  | (List _, _) -> Bool (false)
+    | (Error, _) | (_, Error) -> Error))
 
 (* STRING THINGS *)
 let str_unary_op (op : (string -> string)) : out =
@@ -131,6 +152,25 @@ let str_binary_op (op : (string -> string -> string)) : out =
 let str_concat = str_binary_op (^)
 let str_len = unary_fun from_str String.length to_int
 
+(* LISTS WITH ALL THE CRYPTIC OPERATIONS *)
+let cons =  
+  Fun (fun (o1 : out) -> Fun (fun (o2 : out) -> 
+    match from_list o2 with
+    | Some l -> List (o1::l)
+    | None -> Error))
+let car =
+  Fun (fun (o1 : out) -> 
+    match from_list o1 with
+    | Some (hd::tl) -> hd
+    | Some [] -> Error
+    | None -> Error)
+let cdr =
+  Fun (fun (o1 : out) -> 
+    match from_list o1 with
+    | Some (hd::tl) -> List tl
+    | Some [] -> Error
+    | None -> Error)
+
 (* THIS IS THE STUFF WE CAN DO NOW *)
 let starting_env : env = [(Var "add", int_add); (Var "sub", int_sub);
                           (Var "mult", int_mult); (Var "div", int_div);
@@ -139,7 +179,9 @@ let starting_env : env = [(Var "add", int_add); (Var "sub", int_sub);
                           (Var "and", bool_and); (Var "or", bool_or);
                           (Var "not", bool_not); (Var "eq", eq);
                           (Var "bool_to_str", bool_to_str);
-                          (Var "concat", str_concat); (Var "len", str_len);]
+                          (Var "concat", str_concat); (Var "len", str_len);
+                          (Var "cons", cons); (Var "car", car); 
+                          (Var "cdr", cdr);]
 
 (* NOW THIS IS HOW TO MAKE THINGS HAPPEN *)
 let run (exp : exp) : out =
